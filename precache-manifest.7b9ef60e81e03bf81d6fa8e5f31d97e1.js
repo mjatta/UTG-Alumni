fc37b8f4d1158ffc8f2cdbadd6f547002064ac69
mjatta <30508772+mjatta@users.noreply.github.com>
2020-04-18 21:09:24 -0400

    deploy

---
+++ precache-manifest.7b9ef60e81e03bf81d6fa8e5f31d97e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d478ecb2b24f7368bf",
+    "url": "/UTG-Alumni/css/app.adeb4b83.css"
+  },
+  {
+    "revision": "9e917c5a78c94c32d94f",
+    "url": "/UTG-Alumni/css/chunk-vendors.a7563475.css"
+  },
+  {
+    "revision": "52b9bc134155ef4c5697fd98e93f124a",
+    "url": "/UTG-Alumni/executive.json"
+  },
+  {
+    "revision": "487c9e61805e82f3751d02dbfe710674",
+    "url": "/UTG-Alumni/index.html"
+  },
+  {
+    "revision": "938732f6a6cb228fa4f8",
+    "url": "/UTG-Alumni/js/about.d74b6ebe.js"
+  },
+  {
+    "revision": "31d478ecb2b24f7368bf",
+    "url": "/UTG-Alumni/js/app.91f87048.js"
+  },
+  {
+    "revision": "9e917c5a78c94c32d94f",
+    "url": "/UTG-Alumni/js/chunk-vendors.00d9402d.js"
+  },
+  {
+    "revision": "20e07e62bf50e289f7e487575b2a80e0",
+    "url": "/UTG-Alumni/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/UTG-Alumni/robots.txt"
+  }
+]);
